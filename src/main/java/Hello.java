75e8047905f3d5f3065624c1c658f179270602de
wangxinyuangit <496415493@qq.com>
2019-09-16 11:19:53 +0800

    nihao

---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<html><body>");
+        printWriter.println("<h2>Hello wxy 我是直接自动建立的Servlet!</h2></body></html>");
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<html><body>");
+        printWriter.println("<h2>Hello Wxy 我是手动建立的Servlet!</h2></body></html>");
+    }
+}
